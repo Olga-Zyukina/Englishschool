c41b1d9cd328fae318020118c7127d583e8ffbdf
Olga-Zyukina <bloger_80@mail.ru>
2023-11-09 14:55:14 +0300

    09112023

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,6 @@
 (function() {
     "use strict";
-  
-    /**
-     * Easy selector helper function * селектор
-     */
+
     const select = (el, all = false) => {
       el = el.trim()
       if (all) {
@@ -13,9 +10,6 @@
       }
     }
   
-    /**
-     * Easy event listener function * событие
-     */
     const on = (type, el, listener, all = false) => {
       let selectEl = select(el, all)
       if (selectEl) {
@@ -27,56 +21,38 @@
       }
     }
   
-    /**
-     * Easy on scroll event listener * скролл
-     */
     const onscroll = (el, listener) => {
       el.addEventListener('scroll', listener)
     }
-  
-    /**
-     * Navbar links active state on scroll
-     * активация пункта меню при скролле
-     */
-    let navbarlinks = select('#navbar .scrollto', true)
-    const navbarlinksActive = () => {
-      let position = window.scrollY + 200
-      navbarlinks.forEach(navbarlink => {
-        if (!navbarlink.hash) return
-        let section = select(navbarlink.hash)
-        if (!section) return
-        if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
-          navbarlink.classList.add('active')
-        } else {
-          navbarlink.classList.remove('active')
-        }
-      })
-    }
-    window.addEventListener('load', navbarlinksActive)
-    onscroll(document, navbarlinksActive)
-  
-    /**
-     * Scrolls to an element with header offset
-     */
+   
     const scrollto = (el) => {
-      let header = select('#header')
-      let offset = header.offsetHeight
-  
-      if (!header.classList.contains('header-scrolled')) {
-        offset -= 16
-      }
-  
       let elementPos = select(el).offsetTop
       window.scrollTo({
-        top: elementPos - offset,
+        top: elementPos,
         behavior: 'smooth'
       })
-    }
-  
-    /**
-     * Back to top button
-     * кнопка наверх
-     */
+    } 
+
+    on('click', '.mobile-nav-toggle', function(e) {
+      select('#navbar').classList.toggle('navbar-mobile')
+      this.classList.toggle('bi-list')
+      this.classList.toggle('bi-x')
+    })
+   
+    on('click', '.scrollto', function(e) {
+      if (select(this.hash)) {
+        e.preventDefault()
+        let navbar = select('#navbar')
+        if (navbar.classList.contains('navbar-mobile')) {
+          navbar.classList.remove('navbar-mobile')
+          let navbarToggle = select('.mobile-nav-toggle')
+          navbarToggle.classList.toggle('bi-list')
+          navbarToggle.classList.toggle('bi-x')
+        }
+        scrollto(this.hash)
+      }
+    }, true)
+
     let backtotop = select('.back-to-top')
     if (backtotop) {
       const toggleBacktotop = () => {
@@ -88,50 +64,8 @@
       }
       window.addEventListener('load', toggleBacktotop)
       onscroll(document, toggleBacktotop)
-    }
-  
-    /**
-     * Mobile nav toggle
-     * мобильное меню
-     */
-    on('click', '.mobile-nav-toggle', function(e) {
-      select('#navbar').classList.toggle('navbar-mobile')
-      this.classList.toggle('bi-list')
-      this.classList.toggle('bi-x')
-    })
-  
-    /**
-     * Mobile nav dropdowns activate
-     */
-    on('click', '.navbar .dropdown > a', function(e) {
-      if (select('#navbar').classList.contains('navbar-mobile')) {
-        e.preventDefault()
-        this.nextElementSibling.classList.toggle('dropdown-active')
-      }
-    }, true)
-  
-    /**
-     * Scrool with ofset on links with a class name .scrollto
-     */
-    on('click', '.scrollto', function(e) {
-      if (select(this.hash)) {
-        e.preventDefault()
-  
-        let navbar = select('#navbar')
-        if (navbar.classList.contains('navbar-mobile')) {
-          navbar.classList.remove('navbar-mobile')
-          let navbarToggle = select('.mobile-nav-toggle')
-          navbarToggle.classList.toggle('bi-list')
-          navbarToggle.classList.toggle('bi-x')
-        }
-        scrollto(this.hash)
-      }
-    }, true)
+    }   
    
-    /**
-     * Animation on scroll
-     * инициализация библиотеки aos
-     */
     window.addEventListener('load', () => {
       AOS.init({
         duration: 1000,
